9734bbf90e30ff76b1218fa80237cd7c731f748e
Ayaz Hussein <ayazhussein@gmail.com>
2025-02-17 17:18:53 +0200

    fix build

--- apps/example-api/src/demo-extending.ts
+++ apps/example-api/src/demo-extending.ts
@@ -9,22 +9,23 @@ const model = z.object({
   id: z.string().uuid(),
 });
 type Model = typeof model;
+type ModelSchema = z.infer<typeof model>;
 
 export class DemoExtending extends PrismaService<Model, typeof prisma.user> {
-  create = async (input: unknown) => {
+  create = async (input: ModelSchema) => {
     return this.delegate.findFirstOrThrow({
       where: {
 
       }
     })
   };
-  update = async (id: Id, input: unknown) => {
+  update = async (id: Id, input: ModelSchema) => {
     await this.delegate.update({
       where: {
         id
       },
       data: {
-        email: input['email'],
+        email: input.email
       }
     })
     return this.delegate.findFirstOrThrow({
